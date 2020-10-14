fadd7eeba5abd5f5634c3cadaeca2d2d805d538f
luyuan_cpp@163.com <luyuan_cpp@163.com>
2020-10-14 18:48:38 +0800

    map encoder

--- example/pb3.lua
+++ example/pb3.lua
@@ -15,6 +15,10 @@ print("-------------------")
 for i = 1 , 10 do 
    person.field_map_int32_int32_59:insert(i,i)
 end
+
+for i = 1 , 10 do 
+   person.field_map_int32_int32_59:insert(i,i)
+end
 print("-------------------")
 
 print(person:ByteSize())
@@ -23,8 +27,7 @@ local data = person:SerializeToString()
 local msg = pb3_pb.Proto3MessageWithMaps()
 msg:ParseFromString(data)
 
-print(msg.field_map_bool_bool_1:get(false))
-print(msg.field_map_bool_bool_1:get(true))
+print(msg:ByteSize())
 
 for i = 1 , 10 do 
    print(msg.field_map_int32_int32_59:get(i))
