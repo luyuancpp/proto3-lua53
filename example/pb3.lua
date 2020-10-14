609a12f47afd62cd2ade73a01e14e88f6f560085
luyuan_cpp@163.com <luyuan_cpp@163.com>
2020-10-14 15:45:31 +0800

    add test

---
+++ example/pb3.lua
@@ -0,0 +1,27 @@
+package.path = package.path .. ';../protobuf/?.lua;'
+package.cpath = package.cpath .. ';../protobuf/?.so'
+
+local print = print
+local pb3_pb = require 'pb3_pb'
+local prettyprint = require 'prettyprint'
+
+
+
+local person = pb3_pb.Proto3MessageWithMaps()
+local person1 = pb3_pb.Proto3MessageWithMaps()
+
+
+person.field_map_bool_bool_1:insert(false, false)
+person.field_map_bool_bool_1:insert(true, false)
+
+
+--print(person.field_map_bool_bool_1:get(true))
+--print('==================')
+print(person:ByteSize())
+
+local data = person:SerializeToString()
+local msg = pb3_pb.Proto3MessageWithMaps()
+msg:ParseFromString(data)
+
+print(msg.field_map_bool_bool_1.get(false))
+print(msg.field_map_bool_bool_1.get(true))
