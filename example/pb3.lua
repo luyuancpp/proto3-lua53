be66fb5b82663935af4a52ba996bb3bff9c19d4d
luyuan_cpp@163.com <luyuan_cpp@163.com>
2020-10-15 17:57:38 +0800

    assert message

--- example/pb3.lua
+++ example/pb3.lua
@@ -12,21 +12,20 @@ p3m.field_int32_5 = 5
 person.field_map_bool_bool_1:insert(false, false)
 person.field_map_bool_bool_1:insert(true, false)
 
-print("-------------------")
 for i = 1 , 10 do 
    person.field_map_int32_int32_59:insert(i,i)
 end
 
 person.field_map_int32_message_61:insert(1, p3m)
-print("-------------------")
 
-print(person:ByteSize())
 
 local data = person:SerializeToString()
 local msg = pb3_pb.Proto3MessageWithMaps()
 msg:ParseFromString(data)
 
 print(msg:ByteSize())
+
+assert(msg:ByteSize() == person:ByteSize(), "pb sizer error")
 
 for i = 1 , 10 do 
    print(msg.field_map_int32_int32_59:get(i))
