809acc73a500d4d7a8f5ebc703750579b5be29ee
luyuan_cpp@163.com <luyuan_cpp@163.com>
2020-10-23 15:28:26 +0800

    u 64

--- example/pb3.lua
+++ example/pb3.lua
@@ -7,9 +7,8 @@ local prettyprint = require 'prettyprint'
 
 local p3mwm = pb3_pb.Proto3MessageWithMaps()
 local p3m = pb3_pb.Proto3Message()
-p3m.field_uint64_4 =  ((2 << 61) - 1)
---p3m.field_uint64_4 =  9223372036854775807
-print(string.format("%.u", p3m.field_uint64_4))
+p3m.field_uint64_4 =  ((2 << 61) )
+p3m.field_int64_3 =  ((2 << 60) )
 
 local p3mss = pb3_pb.Proto3Message()
 local p3mdata = p3m:SerializeToString()
@@ -17,4 +16,15 @@ local p3mdata = p3m:SerializeToString()
 p3mss:ParseFromString(p3mdata)
 print(string.format("%.u", p3m.field_uint64_4))
 print(string.format("%.u", p3mss.field_uint64_4))
+
+if p3m.field_uint64_4 == p3mss.field_uint64_4 then
+  print("same")
+end
+
+if p3m.field_uint64_4 == p3mss.field_uint64_4 then
+  print("same")
+end
+
+assert(p3m.field_uint64_4 == p3mss.field_uint64_4, "error")
+assert(p3m.field_int64_3 == p3mss.field_int64_3, "error")
 
