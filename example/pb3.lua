6ca7f00d2b2e38515f82d33d9642a818e9d495fc
luyuan_cpp@163.com <luyuan_cpp@163.com>
2020-10-23 15:31:20 +0800

    u64 test

--- example/pb3.lua
+++ example/pb3.lua
@@ -7,6 +7,7 @@ local prettyprint = require 'prettyprint'
 
 local p3mwm = pb3_pb.Proto3MessageWithMaps()
 local p3m = pb3_pb.Proto3Message()
+p3m.field_int32_5 = 5 
 p3m.field_uint64_4 =  ((2 << 61) )
 p3m.field_int64_3 =  ((2 << 60) )
 
@@ -28,3 +29,28 @@ end
 assert(p3m.field_uint64_4 == p3mss.field_uint64_4, "error")
 assert(p3m.field_int64_3 == p3mss.field_int64_3, "error")
 
+p3mwm.field_map_bool_bool_1:insert(false, false)
+p3mwm.field_map_bool_bool_1:insert(true, false)
+
+for i = 1 , 10 do 
+   p3mwm.field_map_int32_int32_59:insert(i,i)
+end
+
+p3mwm.field_map_int32_message_61:insert(1, p3m)
+
+
+local data = p3mwm:SerializeToString()
+local p3mwmss = pb3_pb.Proto3MessageWithMaps()
+p3mwmss:ParseFromString(data)
+
+print(p3mwmss:ByteSize())
+
+assert(p3mwmss:ByteSize() == p3mwm:ByteSize(), "pb sizer error")
+
+for i = 1 , 10 do 
+   print(p3mwmss.field_map_int32_int32_59:get(i))
+end
+
+
+
+print(p3mwmss.field_map_int32_message_61:get(1).field_int32_5)
