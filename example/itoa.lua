e7fb19126b4d618fa2757d3486c610b5558b3804
luyuan_cpp@163.com <luyuan_cpp@163.com>
2020-10-23 15:47:33 +0800

    test

---
+++ example/itoa.lua
@@ -0,0 +1,3 @@
+local a = ((2 << 63) - 1)
+print(string.format("%22.u", a))
+
--- example/pb3.lua
+++ example/pb3.lua
@@ -5,8 +5,17 @@ local print = print
 local pb3_pb = require 'pb3_pb'
 local prettyprint = require 'prettyprint'
 
+
+local testassert(a, b)
+  if a == b then
+     print("same")
+  end
+  assert(a == b, "error")
+end
+
 local p3mwm = pb3_pb.Proto3MessageWithMaps()
 local p3m = pb3_pb.Proto3Message()
+p3m.field_double_3 = 1.88
 p3m.field_uint64_4 =  ((2 << 61) )
 p3m.field_int64_3 =  ((2 << 60) )
 
@@ -17,14 +26,7 @@ p3mss:ParseFromString(p3mdata)
 print(string.format("%.u", p3m.field_uint64_4))
 print(string.format("%.u", p3mss.field_uint64_4))
 
-if p3m.field_uint64_4 == p3mss.field_uint64_4 then
-  print("same")
-end
+testassert(p3m.field_uint64_4, p3mss.field_uint64_4)
+testassert(p3m.field_uint64_4 == p3mss.field_uint64_4)
 
-if p3m.field_uint64_4 == p3mss.field_uint64_4 then
-  print("same")
-end
-
-assert(p3m.field_uint64_4 == p3mss.field_uint64_4, "error")
-assert(p3m.field_int64_3 == p3mss.field_int64_3, "error")
 
